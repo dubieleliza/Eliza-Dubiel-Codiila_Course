1545c12f2e47939e7e7260abd7850a47646159ef
Eliza Dubiel <dubieleliza89@gmail.com>
2025-09-11 10:37:53 +0200

    Dodano klas Notebook z metodami checkPrice, checkWeight i checkYear

--- kodilla-intro/src/main/java/com/kodilla/FirstClass.java
+++ kodilla-intro/src/main/java/com/kodilla/FirstClass.java
@@ -2,12 +2,25 @@ package com.kodilla;
 
 public class FirstClass {
     public static void main(String[] args) {
-        int age = 1;
 
-        if (age > 1) {
-            System.out.println("Maluszek!");
-        } else if (age <= 3) {
-            System.out.println("Maluch");
-        }
+        Notebook notebook = new Notebook(600, 1000, 2012 );
+        System.out.println(notebook.weight + " " + notebook.price+ " " +notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYear();
+
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2021);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price+ " " +notebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYear();
+
+
+        Notebook oldNotebook = new Notebook(1600, 500, 2025);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price+ " " +notebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYear();
+
     }
 }
