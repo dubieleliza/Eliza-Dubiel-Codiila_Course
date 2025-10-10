3d7f5a3a0348ff6d94a9bb5607c3fa65d1241f7e
Eliza Dubiel <dubieleliza89@gmail.com>
2025-10-10 16:17:13 +0200

    nauczyciele i uczniowie- zadanie z testami

--- kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
@@ -1,4 +1,42 @@
+package com.kodilla.optional.homework;
+
+import org.junit.jupiter.api.Test;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-class ApplicationTest {
-  
+
+public class ApplicationTest {
+
+    @Test
+    public void shouldReturnTeacherNameIfPresent() {
+
+        Teacher teacher = new Teacher("Eliza Dubiel");
+        Student student = new Student("Kuba Wojciechowski", teacher);
+
+        String teacherName = Optional.ofNullable(student.getTeacher())
+                .map(Teacher::getName)
+                .orElse("<undefined>");
+
+        assertEquals("Eliza Dubiel", teacherName);
+    }
+
+    @Test
+    public void shouldReturnUndefinedIfTeacherIsNull() {
+        Student student = new Student("Karolina Jasińska", null);
+
+        String teacherName = Optional.ofNullable(student.getTeacher())
+                .map(Teacher::getName)
+                .orElse("<undefined>");
+
+        assertEquals("<undefined>", teacherName);
+    }
+
+    @Test
+    public void shouldReturnStudentNameCorrectly() {
+        Student student = new Student("Klaudia Kulasza", null);
+
+        String name = student.getName();
+
+        assertEquals("Klaudia Kulasza", name);
+    }
 }
