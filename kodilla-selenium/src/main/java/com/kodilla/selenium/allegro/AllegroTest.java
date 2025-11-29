c375adf74c34e11d933501c2668b13dfb80df6e5
Eliza Dubiel <dubieleliza89@gmail.com>
2025-11-29 11:44:48 +0100

    zadanie z selektorami css - capcha nie przepuszcza

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTest.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTest.java
@@ -1,26 +1,52 @@
+package com.kodilla.selenium.allegro;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class AllegroTest {
-    public static void main(String[] args) throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://allegro.pl/");
 
+    public static void main(String[] args) {
 
-        WebElement searchField = driver.findElement(By.cssSelector("input[name='string']"));
-        searchField.sendKeys("Mavic mini");
+        WebDriver driver = new ChromeDriver();
 
+        try {
+            driver.manage().window().maximize();
+            driver.get("https://allegro.pl/");
 
-        WebElement scopeSelect = driver.findElement(By.cssSelector("select[data-role='search-scope-select']"));
-        Select select = new Select(scopeSelect);
-        select.selectByVisibleText("Elektronika");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+            WebElement searchField = driver.findElement(By.cssSelector("input[name='string']"));
+            searchField.sendKeys("Mavic mini");
 
-        searchField.submit();
+            WebElement scopeSelect = driver.findElement(By.cssSelector("select[data-role='search-scope-select']"));
+            Select select = new Select(scopeSelect);
+            select.selectByVisibleText("Elektronika");
+
+            searchField.submit();
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.cssSelector("section article")
+            ));
+
+            List<WebElement> productCards = driver.findElements(By.cssSelector("section > article"));
+
+            System.out.println("Znaleziono produktów: " + productCards.size());
+
+            for (WebElement product : productCards) {
+                System.out.println("----------------------------------");
+                System.out.println(product.getText());
+            }
+
+        } finally {
+            driver.quit();
+        }
     }
 }
